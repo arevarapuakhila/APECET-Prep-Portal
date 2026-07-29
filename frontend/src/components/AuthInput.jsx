df1f780bc682e18bb5f7e6a875b7dcfa6c6eed56
Arevarapu Akhila <arevarapuakhila@gmail.com>
2026-07-29 23:57:59 +0530

    Day 5:Build login form UI with reusable AuthInput component

---
+++ frontend/src/components/AuthInput.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/authInput.css"
+
+const AuthInput = ({ label, type , placeholder , value , onChange }) => {
+  return (
+    <div className="input-group">
+        <label>{label}</label>
+        <input 
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        />
+    </div>
+  )
+}
+
+export default AuthInput;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import AuthInput from '../components/AuthInput';
+import "../styles/login.css";
+
+import { useState } from "react";
 
 const Login = () => {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] =useState("");
+  const [error, setError] = useState("")
+  
+  console.log(email)
+  console.log(password)
+   
+  const handleSubmit = () => {
+     if (email === ""){
+      setError("Email is required")
+      return;
+     }
+     if (password === ""){
+      setError("Password is required")
+      return;
+     }
+     setError("")
+     console.log("Validation passed")
+  };
   return (
-    <div>Login Page</div>
+    <div className="login-container">  
+      <div className="login-card">
+      <AuthInput 
+       label="Email"
+       type="email"
+       placeholder="Enter your email"
+       value={email}
+       onChange={(e) => setEmail(e.target.value)}
+       />
+       <AuthInput
+        label="Password"
+        type="password"
+        placeholder="Enter your password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p className="error-message">{error}</p>}
+        <button onClick={handleSubmit}>Login</button>
+    </div>
+    </div>
   )
 }
 
 export default Login;
+
+
+
